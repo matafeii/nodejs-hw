363124c708a95bfb40f82e4d4797bc59f2ed1ff4
Kit_Kat <matafeii38@gmail.com>
2026-07-09 21:49:34 +0300

    add notes validation pagination filters

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,3 +1,4 @@
+import { celebrate } from 'celebrate';
 import { Router } from 'express';
 
 import {
@@ -7,13 +8,19 @@ import {
   getNoteById,
   updateNote,
 } from '../controllers/notesController.js';
+import {
+  createNoteSchema,
+  getAllNotesSchema,
+  noteIdSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
 
 const notesRoutes = Router();
 
-notesRoutes.get('/notes', getAllNotes);
-notesRoutes.get('/notes/:noteId', getNoteById);
-notesRoutes.post('/notes', createNote);
-notesRoutes.delete('/notes/:noteId', deleteNote);
-notesRoutes.patch('/notes/:noteId', updateNote);
+notesRoutes.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+notesRoutes.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
+notesRoutes.post('/notes', celebrate(createNoteSchema), createNote);
+notesRoutes.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
+notesRoutes.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default notesRoutes;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 
+import { errors } from 'celebrate';
 import cors from 'cors';
 import express from 'express';
 
@@ -18,6 +19,7 @@ app.use(cors());
 
 app.use(notesRoutes);
 
+app.use(errors());
 app.use(notFoundHandler);
 app.use(errorHandler);
 
